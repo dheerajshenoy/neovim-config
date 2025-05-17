620b801ebb4320f6a20790fb59c472c57081229d
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-05-17 21:37:50 +0530

    Color change

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -2,19 +2,28 @@ vim.o.statusline = "%#StatusLineMode#"
 .. " %{%v:lua.MyMode()%} "
 .. "%#StatusLineFile#"
 .. "%q %t %m %r%="
+.. "%#StatusLineFileType#"
+.. " %{%v:lua.GetFileType()%} "
 .. "%#StatusLinePos#"
 .. " %p%% "
 
-local fg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
-local bg = vim.api.nvim_get_hl(0, { name = "Function" }).fg
+local color_normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+local color_func = vim.api.nvim_get_hl(0, { name = "Function" })
+local color_visual = vim.api.nvim_get_hl(0, { name = "Visual" })
 
 vim.api.nvim_set_hl(0, "StatusLineMode", {
-    bg = bg,
-    fg = fg,
+    bg = color_func.fg,
+    fg = color_normal.bg,
+})
+
+vim.api.nvim_set_hl(0, "StatusLineFileType", {
+    fg = color_normal.fg,
+    bg = color_visual.bg,
 })
 
 vim.api.nvim_set_hl(0, "StatusLineFile", {
-    fg = bg,
+    fg = color_func.fg,
+    bg = color_visual.fg
 })
 
 function _G.MyMode ()
@@ -33,4 +42,8 @@ end
 
 function _G.GitBranch ()
     return git.get_branch()
+end
+
+function _G.GetFileType ()
+    return vim.bo.filetype
 end
